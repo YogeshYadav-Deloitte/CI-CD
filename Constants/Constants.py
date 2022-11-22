a1f08d944eedc0c83ef166c29ef32c039300cbe8
YogeshYadav-Deloitte <101085364+YogeshYadav-Deloitte@users.noreply.github.com>
2022-11-22 13:16:38 +0530

    Web Automation only

--- Constants/Constants.py
+++ Constants/Constants.py
@@ -1,6 +1,7 @@
-from keyring.testing.util import random_string
+# from keyring.testing.util import random_string
 
 URL = "https://open.rocket.chat/"
 NEW_NAME = "CustomUsername1234689"
 STATUS = "Online"
-NEW_CHANNEL_NAME = random_string(10)
+# NEW_CHANNEL_NAME = random_string(10)
+NEW_CHANNEL_NAME = "Random_Name"
